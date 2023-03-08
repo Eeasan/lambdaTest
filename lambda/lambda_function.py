e498d13cff8ce948864ebae294fa7eeb54e4cb8e
zonghao.liu <86272916+Eeasan@users.noreply.github.com>
2023-03-08 17:39:44 +0800

    test2

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,8 +2,8 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    # 第一次修改
+    # 第2次修改
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Github123!')
+        'body': json.dumps('Hello from Github2!')
     }
