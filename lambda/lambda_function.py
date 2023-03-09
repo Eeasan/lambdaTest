038c9352083451065c72abd7a1a6af5df7ec6f85
liu-zonghao-ssoft <zonghao.liu@startiasoft.com>
2023-03-09 08:37:02 +0800

    test5

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -5,5 +5,5 @@ def lambda_handler(event, context):
     # 第2次修改
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Github2!')
+        'body': json.dumps('Hello from Github3!')
     }
